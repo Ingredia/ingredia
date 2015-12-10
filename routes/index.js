cac54568b2fb89262ffdbfa15deb41ae4347ce1b
ingredia <yanlin.wong@foxmail.com>
2015-12-10 15:25:19 +0800

    Add some new files

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ingredia' });
 });
 
+// Deal with 404 and 500.
+router.use(function(req, res, next) {
+    res.status(404);
+    res.render('code', {
+        statusCode: '404',
+        errorState: 'Page Not Found.'
+    });
+});
+
+router.use(function(req, res, next) {
+    res.status(500);
+    res.render('code', {
+        statusCode: '500',
+        errorState: 'Unknown Error.'
+    });
+});
+
 module.exports = router;
