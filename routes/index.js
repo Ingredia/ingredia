a23ad31cf51874e27393cbce129462aa0d1e4103
ingredia <yanlin.wong@foxmail.com>
2015-12-10 15:25:19 +0800

    Revise the header

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ingredia' });
 });
 
+// Deal with 404 and 500.
+// Catch 404
+router.use(function(req, res, next) {
+    res.status(404);
+    res.render('code', {
+        statusCode: '404',
+        errorState: 'Page Not Found.'
+    });
+});
+
+// Catch 500
+router.use(function(req, res, next) {
+    res.status(500);
+    res.render('code', {
+        statusCode: '500',
+        errorState: 'Unknown Error.'
+    });
+});
+
 module.exports = router;
